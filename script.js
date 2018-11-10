c0a9ef78bae838d42bd3ed4d89c93424d455b861
Bolaji <rotibi.m@husky.neu.edu>
2018-11-10 13:20:07 -0500

    some js changes
    Stuff

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.361145, lng: -71.057083},
-          zoom: 8
+          zoom: 10
         });
       }
